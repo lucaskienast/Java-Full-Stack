5cff1e2f97a7131226a5ebae450c222f50d6eb71
Lucas Kienast <80713721+lucaskienast@users.noreply.github.com>
2021-10-02 07:11:00 +0000

    Add files via upload
    Upload of shopping cart react app

---
+++ React Projects/Shopping-Cart-React/src/components/ShoppingCart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ShoppingCart = ( { cartItems, removeItem } ) => {
+    return (
+        <div className="shoppingCart">
+            {cartItems.map((item) => (
+                <div key={item.id} className="cardRow">
+                    <div className="cardImg">
+                        <p>{item.cartCount}</p>
+                        <img src={item.img} alt="shoe" />
+                    </div>
+                    <div className="cardPrice">
+                        <p>{item.price}$</p>
+                        <i 
+                            onClick={() => removeItem(item.id)} 
+                            className="fas fa-times"
+                        ></i>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ShoppingCart;
